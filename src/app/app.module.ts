751671db4dbddf41d1251f6386554c1e1063cceb
Alex Buznik <shu@buznik.net>
2020-10-14 16:29:59 +0300

    Upgrade to Angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayRulesComponent } from './display-rules/display-rules.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayRulesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-rules/display-rules.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-rules',
+  templateUrl: './display-rules.component.html',
+  styleUrls: ['./display-rules.component.sass']
+})
+export class DisplayRulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
